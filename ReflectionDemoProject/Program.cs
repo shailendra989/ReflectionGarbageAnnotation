e82286f9aa803f157419e8ca8a82ca0e318d3d2c
Shailendra kumar pal <palshailendrakumar123@gmail.com>
2023-01-30 23:32:30 +0530

    first commit

---
+++ ReflectionDemoProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ReflectionDemoProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to reflection Demo");
+            ReflectionOnCustomer.ReflectionTest();
+           
+        }
+    }
+}
